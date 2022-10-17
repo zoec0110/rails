7f8df9e0bc7bbd7d316201f7730215fcd00bccd0
Yin <zoe@statementdog.com>
2022-10-17 14:11:50 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_14_025015) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_060829) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -19,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_025015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
   create_table "people", force: :cascade do |t|
