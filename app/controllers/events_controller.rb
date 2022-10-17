cbb82ac8e2f359b11df39b7df553716ff4f5eca8
Yin <zoe@statementdog.com>
2022-10-17 12:35:15 +0800

    create EventsController

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,51 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @page_title = @event.name
+  end
+
+  def edit; end
+
+  def update
+    if @event.update(event_params)
+      flash[:notice] = 'event was successfully updated'
+      redirect_to action: :show, id: @event
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    flash[:alert] = 'event was successfully deleted'
+    redirect_to action: :index
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = 'event was successfully created'
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/say_hello' => 'welcome#say'
   get 'welcome' => 'welcome#index'
   root to: 'welcome#index'
+  match ':controller(/:action(/:id(.:format)))', via: :all
   # Defines the root path route ("/")
   # root "articles#index"
 end
